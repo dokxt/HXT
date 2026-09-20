4d4330600c357a7a28750facfb523fa17e976dd6
dokxt <dodisiok248@gmail.com>
2026-09-20 18:30:04 -0500

    Implement greeting function and button click handler

--- c.js
+++ c.js
@@ -1,1 +1,13 @@
+// A function = a recipe. Write it once, use it many times.
+function sayHello(name) {
+  return "Hello, " + name + "!";
+}
 
+// Find the elements from the HTML (using their id)
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+
+// When the button is clicked, do something:
+button.addEventListener("click", () => {
+  message.textContent = sayHello("world") + " Time: " + new Date().toLocaleTimeString();
+});
